64030f91bf53dee3475c334864939aff5a6d13e0
An72ty <An72ty@gmail.com>
2023-07-27 22:42:56 +0500

    /

--- plugin.py
+++ plugin.py
@@ -1,59 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import annotations
-from rich.console import Console
 from PIL import Image
-import importlib
-# Import system ptools
-import libs.ptools as ptools
-# Create globals
-AppData = None
-console = Console()
-
-modulesNames = ['errors', 'commandManager']
-NAME = "image viewer"
-
-# Init fuction init AppData
+from PyQt5.QtWidgets import QFileDialog
 
 
-def init(data):
-    global AppData
-    AppData = data
+class Window():
+    def __init__(self):
+        self.filename, _ = QFileDialog.getOpenFileName()
+        if self.filename:
+            with Image.open(self.filename) as self.img:
+                self.img.load()
 
-
-class NotAllModulesConnected(Exception):
-    pass
-
-
-class p(ptools.Plugin):
-    def __init__(
-        self,
-        data,
-        modules: list = ...
-    ) -> p:
-        super().__init__(data, modules)
-        connectedModulesNames = [module.NAME for module in self.modules]
-        if connectedModulesNames == modulesNames:
-            self.errors.Functions.debug('Connected plugin {0}'.format(NAME))
-        else:
-            raise NotAllModulesConnected('Connected modules: {0} Not connected: {1}'.format(
-                connectedModulesNames, modulesNames))
-
-    def activate(self) -> bool:
-        self.errors.Functions.debug('Plugin {0} activated'.format(NAME))
-        AppData.groups.list['System tools'].addCommand([
-            self.commandManager.Command('view_image', self.view_image, 'View image')])
-        AppData.groups.update()
-
-    def view_image(self):
-        self.errors.Functions.debug('Invoked function view_image')
-        while True:
-            console.print('[white]Enter the path of the image (e - cancel)')
-            console.print('[red]>[/] ', end='')
-            answer = input().strip()
-            if answer.lower() == 'e':
-                console.print('[white]You cancelled[/]')
-                break
-            else:
-                with Image.open(answer) as img:
-                    img.load()
-                img.show()
+    def show(self):
+        self.img.show()
